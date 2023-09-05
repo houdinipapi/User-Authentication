b16cb65546b18e4f56f737bade7c01755065f62c
papihoudini <ochiengcliff.co@gmail.com>
2023-09-05 03:13:50 -0400

    Creating instances

--- auth-app/app.py
+++ auth-app/app.py
@@ -11,3 +11,29 @@ from flask_login import (
     logout_user,
     login_required,
 )
+
+# Creating an instance for flask_login
+login_manager = LoginManager()
+login_manager.session_protection = "strong"
+login_manager.login_view = "login"
+login_manager.login_message_category = "info"
+
+# Other Instances
+db = SQLAlchemy()
+migrate = Migrate()
+bcrypt = Bcrypt()
+
+# Creating Flask instance within a function --> allows for multiple application instances
+def create_app():
+    app = Flask(__name__)
+
+    app.secret_key = "secret-key"
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
+
+    login_manager.init_app(app)
+    db.init_app(app)
+    migrate.init_app(app, db)
+    bcrypt.init_app(app)
+
+    return app
